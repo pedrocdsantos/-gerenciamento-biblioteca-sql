c2b208319841d9132e6153dd075c61ea0b3701fd
Pedro <pedro37066@gmail.com>
2025-03-28 20:51:54 -0300

    insert de dados

---
+++ inserçaodados.sql
@@ -0,0 +1,6 @@
+INSERT INTO categoria (nome, descricao) VALUES
+('Romance', 'Livros que abordam histórias de amor e relacionamentos.'),
+('Ficção', 'Livros com histórias criativas e imaginárias.'),
+('Clássico', 'Livros consagrados pela crítica e pelo público.'),
+('Mistério', 'Livros com histórias de suspense e investigação.'),
+('Filosofia', 'Livros que tratam de temas filosóficos e reflexivos.');
